c1a72ece481c77a04033597d5588bd81e1dde3e9
FussyPucker <61292951+FussyPucker@users.noreply.github.com>
2020-02-20 18:03:50 -0500

    Update Firstmotor.ino

--- Firstmotor.ino
+++ Firstmotor.ino
@@ -8,6 +8,7 @@
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
+int pos = 0;    // variable to store the servo position
 int E1 = 6; //Inittial the output pin
 int M1 = 7;
 int E2 = 5;
@@ -23,6 +24,17 @@ void setup() {
   pinMode (M2, INPUT);
 }
 
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
 void loop() 
 {
   int value=155;
