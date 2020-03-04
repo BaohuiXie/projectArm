78227280f31924387e471b3e65aca3ba015a256e
BaohuiXie <43047509+BaohuiXie@users.noreply.github.com>
2020-03-03 21:32:32 -0500

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,18 +1,24 @@
-#include <Servo.h>
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-int pos = 0;    // variable to store the servo position
-void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+//-------------------------------------------------------------------------------------------------------------
+//configuration for sensor
+int sensorValue1 = 0; // the value returned from the analog sensor, between 0 and 1023
+int analog1INPUTPIN = A0; // the analog pin that the sensor is on
+int angle1;
+
+
+void setup() { 
+   //sensor
+    //Shoulder joint sensor
+    pinMode(A0,INPUT);   //select the input pin for potentiometer
+       //value set up
+    Serial.begin(9600); // Set up the serial connection for printing, start communication at 9600 baud
 }
+
+
+
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+  //read sensor section
+  sensorValue1 = analogRead(analog1INPUTPIN);
+  angle1 = map(sensorValue1,0,1023, 0,240);
+  Serial.print("angle value: ");
+  Serial.println(angle1);
 }
