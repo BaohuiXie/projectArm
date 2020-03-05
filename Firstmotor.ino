295bc4c3ab7f05d2fdc8afba5445241123fe9843
FussyPucker <61292951+FussyPucker@users.noreply.github.com>
2020-03-04 22:28:19 -0500

    Update Firstmotor.ino

--- Firstmotor.ino
+++ Firstmotor.ino
@@ -1,27 +1,54 @@
+//motor 从背面看过去 counterclockwise rotating means shoulder自身角度来说counterclockwise
 //userInput value----------------------------------------------------------------------------------------------
-int userInputAngle1=10;
+int userInBack=10;
+int userInShoulder=10;
+
+
+
+
+//---------------------------------------------------------------------------------------------------------------
+//offset range of the sensor (range of each joint)
+//back
+int minBack;
+int maxBack;
+//shoulder
+int minShoulder;
+int maxShoulder;
 
 
 
 //--------------------------------------------------------------------------------------------------
 //configuration for H bridge (controlling direction)
+//Shoulder
 int In1=7;
 int In2=8;
 int ENA=9;
+//Back
+int In3=2;
+int In4=4;
+int ENB=3;
 
 
 
 //-------------------------------------------------------------------------------------------------------------
 //configuration for sensor
-int sensorValue1 = 0; // the value returned from the analog sensor, between 0 and 1023
-int analog1INPUTPIN = A0; // the analog pin that the sensor is on
+//shoulder
+int sensorValueShoulder = 0; // the value returned from the analog sensor, between 0 and 1023
+int analogInShoulder = A0; // the analog pin that the sensor is on
+//Back
+int sensorValueBack = 0; // the value returned from the analog sensor, between 0 and 1023
+int analogInBack = A1; // the analog pin that the sensor is on
 
 
 
 //---------------------------------------------------------------------------------------------------------------
 //some value to be state
-int angle1;
-int rotationState1;   //0: not raising, 1: raising
+//shoulder
+int angleShoulder;
+int rotationStateShoulder;   //0: clockwise (not raising), 1: Counterclockwise (rising)
+//Back
+int angleBack;
+int rotationStateBack; //0: clockwise (not raising), 1: Counterclockwise (rising)
 
 
 
@@ -30,13 +57,19 @@ int rotationState1;   //0: not raising, 1: raising
 void setup() { 
    //sensor
     //Shoulder joint sensor
-    pinMode(A0,INPUT);   //select the input pin for potentiometer
+    pinMode(A0, INPUT);   //select the input pin for potentiometer
+    //Back joint sensor
+    pinMode(A1, INPUT);
 
    //H bridge
     //Shoulder joint Motor
     pinMode(ENA, OUTPUT);
     pinMode(In1, OUTPUT);
     pinMode(In2, OUTPUT);
+    //Back joint Motor
+    pinMode(ENB, OUTPUT);
+    pinMode(In3, OUTPUT);
+    pinMode(In4, OUTPUT);
 
    //value set up
     Serial.begin(9600); // Set up the serial connection for printing, start communication at 9600 baud
@@ -44,46 +77,67 @@ void setup() {
 
 void loop() {
   //read sensor section
-  sensorValue1 = analogRead(analog1INPUTPIN);
-  angle1 = map(sensorValue1,0,1023, 0,240);
- 
+  sensorValueBack = analogRead(analogInBack);
+  angleBack = map(sensorValueBack,0,1023, 0,240);
+  sensorValueShoulder = analogRead(analogInShoulder);
+  angleShoulder = map(sensorValueShoulder,0,1023, 0,240);
+
+
   
   //determine state of arm rotation
-  rotationState1 = stateOfRotation(angle1,userInputAngle1);
-  
-  Serial.print("state of rotation: ");
-  Serial.println(rotationState1);
-  rotationOperator(rotationState1, angle1, sensorValue1, analog1INPUTPIN, userInputAngle1);
+  rotationStateShoulder = stateOfRotation(angleShoulder,userInShoulder);
+  rotationStateBack = stateOfRotation(angleBack,userInBack);
 
+
+
+
+  //check state section
+  Serial.print("state of Shoulder rotation: ");
+  Serial.println(rotationStateShoulder);
+  delay(3);
+  Serial.print("state of Back rotation: ");
+  Serial.println(rotationStateShoulder);
+  delay(3);
+
+
+
+// speed is set to 150 in the midterm demo
+
+  //operating section
+  rotationOperator(rotationStateBack, angleBack, sensorValueBack, analogInBack,  In3, In4, ENB, 150, userInBack, minBack, maxBack);
+  rotationOperator(rotationStateShoulder, angleShoulder, sensorValueShoulder, analogInShoulder,  In1, In2, ENA, 150, userInShoulder, minShoulder, maxShoulder);
+  if(angleBack == userInBack || angleShoulder == userInShoulder){
+      while(1){};  
+  }
 }
 
 
 
 //---------------------------------------------------------------------------------------------
 //opertating the rotation for diferent motor or joint with different case
-void rotationOperator(int roState, int angl, int senValue, int motorSignalPin, int userIn){
+void rotationOperator(int roState, int angl, int senValue, int analogInPin, int pinNumber1, 
+              int pinNumber2, int enNumber, int enSpeed, int userIn, int minRange, int maxRange){
    switch (roState){
-    case 1:
-      while (angl > 0 && angl < userIn) { 
-        senValue = analogRead(motorSignalPin);
+    case 1:                                                                                // Counterclockwise (rising)
+      while ((angl > minRange) && (angl < maxRange) && (angl < userIn)) { 
+        senValue = analogRead(analogInPin);
         angl = map(senValue,0,1023,0,240);
-        
+        counterClockWiseRotate(pinNumber1,pinNumber2,enNumber,enSpeed);
+        delay(3);
       }
       break;
-    case 0:
-      while (angl > userIn && angl > 0) { 
-        senValue = analogRead(motorSignalPin);
+    case 0:                                                                                // clockwise (no rising)
+      while ((angl > minRange) && (angl < maxRange) && (angl > userIn)) { 
+        senValue = analogRead(analogInPin);
         angl = map(senValue,0,1023,0,240);
-        
+        clockWiseRotate(pinNumber1,pinNumber2,enNumber,enSpeed);
+        delay(3);
         }
        break;
      default:                      //rest
       
       break;
-    }    
-    Serial.print("angle: ");
-    Serial.println(angl);
-    while(1){};
+    }   
 }
 
 
@@ -109,17 +163,19 @@ void turnOffMotor(int pinNumber1, int pinNumber2){
 
 
 //------------------------------------------------------------------------------------------
+//从马达背面看过去
 void clockWiseRotate(int pinNumber1, int pinNumber2, int enNumber, int enSpeed){
   analogWrite(enNumber, enSpeed);
-  digitalWrite(pinNumber1, HIGH);         //这个要验证
-  digitalWrite(pinNumber2, LOW);
+  digitalWrite(pinNumber1, LOW);         
+  digitalWrite(pinNumber2, HIGH);
 }
 
 
 
 //------------------------------------------------------------------------------------------
-void clockWiseRotate(int pinNumber1, int pinNumber2,int enNumber, int enSpeed){
+//从马达背面看过去
+void counterClockWiseRotate(int pinNumber1, int pinNumber2, int enNumber, int enSpeed){
    analogWrite(enNumber, enSpeed);
-  digitalWrite(pinNumber1, LOW);         //这个要验证
-  digitalWrite(pinNumber2, HIGH);
+   digitalWrite(pinNumber1, HIGH);         
+   digitalWrite(pinNumber2, LOW);
 }
